c15c5de94d90172a98052609f914ac0f4da7ec15
Marcos <marcostapia4@gmail.com>
2021-06-06 12:16:18 +0000

    Forgot to add leftshift DSP

---
+++ verilog/hardware/processor/improved_verilog/leftshift_dsp.v
@@ -0,0 +1,90 @@
+/*
+	Authored 2018-2019, Ryan Voo.
+
+	All rights reserved.
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions
+	are met:
+
+	*	Redistributions of source code must retain the above
+		copyright notice, this list of conditions and the following
+		disclaimer.
+
+	*	Redistributions in binary form must reproduce the above
+		copyright notice, this list of conditions and the following
+		disclaimer in the documentation and/or other materials
+		provided with the distribution.
+
+	*	Neither the name of the author nor the names of its
+		contributors may be used to endorse or promote products
+		derived from this software without specific prior written
+		permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+	FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+	COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+	BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+	LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+	ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+
+/*
+ *	Description:
+ *
+ *		This module implements an left shift register for use by the ALU
+ */
+
+
+
+module leftshift_dsp(input1, input2, out);
+	input[31:0] input1;
+	input[31:0] input2;
+	output[31:0] out;
+
+	SB_MAC16 i_sbmac16(  
+		// port interfaces
+		.A(input1[15:0]), // 16x16 mulitplier only
+		.B(input2[15:0]),
+		.C(input1[31:16]),
+		.D(input2[31:16]),
+		.O(out),
+		.CLK(), 	// Asynchronous
+		.CE(1'b0),
+		.IRSTTOP(1'b0), // Default resets are 0 but doesn't matter since unclocked
+		.IRSTBOT(1'b0),
+		.ORSTTOP(1'b0),
+		.ORSTBOT(1'b0),
+		.AHOLD(1'b0),  // Default holds are 0 but doesn't matter since unclocked
+		.BHOLD(1'b0),
+		.CHOLD(1'b0),
+		.DHOLD(1'b0),
+		.OHOLDTOP(1'b0),
+		.OHOLDBOT(1'b0),
+		.OLOADTOP(1'b0),
+		.OLOADBOT(1'b0),
+		.ADDSUBTOP(1'b0), // 0 for add (default)
+		.ADDSUBBOT(1'b0),
+		.CO(),
+		.CI(1'b0), // Default
+		//MAC cascading ports.
+		.ACCUMCI(),
+		.ACCUMCO(),
+		.SIGNEXTIN(1'b0), // Unsigned
+		.SIGNEXTOUT()
+	);
+
+	defparam i_sbmac16.TOPOUTPUT_SELECT = 2'b11;
+	defparam i_sbmac16.BOTOUTPUT_SELECT = 2'b11;
+	//defparam i_sbmac16.PIPELINE_16x16_MULT_REG2 = 1'b1;
+	defparam i_sbmac16.A_SIGNED= 1'b0;
+	defparam i_sbmac16.B_SIGNED	= 1'b0;
+
+	endmodule
